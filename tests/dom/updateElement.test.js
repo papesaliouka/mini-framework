b1f8a7da1f4e208950d38c25fdfc404181414763
papesaliouka <papesaliouka@gmail.com>
2024-01-27 09:03:48 +0000

    added other test cases

--- tests/dom/updateElement.test.js
+++ tests/dom/updateElement.test.js
@@ -1,1 +1,29 @@
-import { updateElement } from '../../src/core/dom/updateElement';
+
+import { updateElement } from '../dom/updateElement';
+import { createElement } from '../dom/createElement';
+
+// tests/dom/updateElement.test.js
+
+describe('updateElement', () => {
+    let element;
+
+    beforeEach(() => {
+        element = createElement('div');
+    });
+
+    it('updates text content', () => {
+        updateElement(element, { text: "Hello World" });
+        expect(element.textContent).toBe("Hello World");
+    });
+
+    it('updates styles', () => {
+        updateElement(element, { style: { color: 'red' } });
+        expect(element.style.color).toBe('red');
+    });
+
+    it('updates attributes', () => {
+        updateElement(element, { attributes: { 'data-test': 'value' } });
+        expect(element.getAttribute('data-test')).toBe('value');
+    });
+});
+
